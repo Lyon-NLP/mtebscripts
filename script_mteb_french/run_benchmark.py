604602731f02fec1921bdbd8382a0ffcc183bf3f
Imene Kerboua <imenelydia.kr@gmail.com>
2024-01-23 14:04:11 +0100

    Eval AlloProf and others part 5

--- script_mteb_french/run_benchmark.py
+++ script_mteb_french/run_benchmark.py
@@ -24,8 +24,32 @@ Example: MODELS = [ModelConfig("intfloat/multilingual-e5-base", model_type="sent
 # Step 1 : Setup model list #
 #############################
 SENTENCE_TRANSORMER_MODELS = [
-    #"izhx/udever-bloom-560m",
-    #"izhx/udever-bloom-1b1",
+    "bert-base-multilingual-cased",
+    "bert-base-multilingual-uncased",
+    "flaubert/flaubert_base_uncased",
+    "flaubert/flaubert_base_cased",
+    "flaubert/flaubert_large_cased",
+    "dangvantuan/sentence-camembert-base",
+    "sentence-transformers/distiluse-base-multilingual-cased-v2",
+    "sentence-transformers/all-MiniLM-L6-v2",
+    "sentence-transformers/all-MiniLM-L12-v2",
+    "sentence-transformers/LaBSE",
+    "sentence-transformers/multi-qa-MiniLM-L6-cos-v1",
+    "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+    "intfloat/multilingual-e5-base",
+    "intfloat/multilingual-e5-large",
+    "intfloat/multilingual-e5-small",
+    "distilbert-base-uncased",
+    "Geotrend/distilbert-base-25lang-cased",
+    "Geotrend/distilbert-base-en-fr-es-pt-it-cased",
+    "Geotrend/distilbert-base-en-fr-cased",
+    "Geotrend/distilbert-base-fr-cased",
+    "Geotrend/bert-base-25lang-cased",
+    "Geotrend/bert-base-15lang-cased",
+    "Geotrend/bert-base-10lang-cased",
+    "shibing624/text2vec-base-multilingual",
+    "izhx/udever-bloom-560m",
+    "izhx/udever-bloom-1b1",
     "sentence-transformers/sentence-t5-base",
     "sentence-transformers/sentence-t5-large",
     "sentence-transformers/sentence-t5-xl",
@@ -42,11 +66,11 @@ SENTENCE_TRANSORMER_MODELS = [
 
 # these models max_length is indicated to be 514 whereas the embedding layer actually supports 512
 SENTENCE_TRANSORMER_MODELS_WITH_ERRORS = [
-    #"camembert/camembert-base",
-    #"camembert/camembert-large",
-    #"dangvantuan/sentence-camembert-large",
-    #"xlm-roberta-base",
-    #"xlm-roberta-large",
+    "camembert/camembert-base",
+    "camembert/camembert-large",
+    "dangvantuan/sentence-camembert-large",
+    "xlm-roberta-base",
+    "xlm-roberta-large",
 ]
 
 UNIVERSAL_SENTENCE_ENCODER_MODELS = [
