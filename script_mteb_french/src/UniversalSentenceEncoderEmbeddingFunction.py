92f4331f0b8587708809f3223a0147314b1b133e
Wissam Siblini <wissam.siblini92@gmail.com>
2024-02-23 15:41:36 +0100

    Adding truncation

--- script_mteb_french/src/UniversalSentenceEncoderEmbeddingFunction.py
+++ script_mteb_french/src/UniversalSentenceEncoderEmbeddingFunction.py
@@ -34,7 +34,7 @@ class UniversalSentenceEncoderEmbeddingFunction(AbstractEmbeddingFunction):
         return self._model_name
 
     def encode_documents(self, input: Documents) -> Embeddings:
-        truncated_documents = [" ".join(x.split(' ')) for x in input]
+        truncated_documents = [" ".join(x.split(' ')[:self.max_token_length]) for x in input]
         return self.model(truncated_documents).numpy().tolist()
     
     @classmethod
