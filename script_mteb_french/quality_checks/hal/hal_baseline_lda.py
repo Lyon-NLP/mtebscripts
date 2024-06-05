0e1ba7e3032f9c700b5a255067b7d87b69dc98fd
Imene Kerboua <imenelydia.kr@gmail.com>
2024-06-05 12:59:53 +0200

    update lda script

--- script_mteb_french/quality_checks/hal/hal_baseline_lda.py
+++ script_mteb_french/quality_checks/hal/hal_baseline_lda.py
@@ -2,6 +2,7 @@ from datasets import load_dataset
 
 import spacy
 import nltk
+import spacy.cli
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
@@ -13,30 +14,30 @@ DATASET = "lyon-nlp/clustering-hal-s2s"
 SEED = 42
 STOPWORDS = stopwords.words("french") # + stopwords.words("english") 
 
+try:
+    nlp = spacy.load('fr_core_news_sm', disable = ['parser','ner'])
+except FileNotFoundError:
+    spacy.cli.download("fr_core_news_sm")
+    nlp = spacy.load('fr_core_news_md', disable = ['parser','ner'])
+
 
 dataset = load_dataset(DATASET, name="mteb_eval", split="test")
 dataset = dataset.class_encode_column("domain")
 num_classes = dataset.features["domain"].num_classes
 id2label = {k: dataset.features["domain"].int2str(k) for k in range(num_classes)}
-dataset = dataset.train_test_split(test_size=0.3, shuffle=True, stratify_by_column="domain", seed=SEED)
 
-X_train, y_train = dataset["train"]["title"], dataset["train"]["domain"]
-X_test, y_test = dataset["test"]["title"], dataset["test"]["domain"]
+texts, domains = dataset["title"], dataset["domain"]
 
-nlp = spacy.load('fr_core_news_sm', disable = ['parser','ner'])
-
-docs_train = nlp.pipe(X_train)
-docs_test = nlp.pipe(X_test)
+docs = nlp.pipe(texts)
 
 def tokenize_text(doc):
     return [token.lemma_.lower() for token in doc if token not in STOPWORDS]
 
-tokenized_X_train = [tokenize_text(doc) for doc in docs_train]
-tokenized_X_test = [tokenize_text(doc) for doc in docs_test]
-print(tokenized_X_train[:5])
+tokenized_texts = [tokenize_text(doc) for doc in docs]
+print(tokenized_texts[:5])
 
-common_dictionary = Dictionary(tokenized_X_train)
-common_corpus = [common_dictionary.doc2bow(text) for text in tokenized_X_train]
+common_dictionary = Dictionary(tokenized_texts)
+common_corpus = [common_dictionary.doc2bow(text) for text in tokenized_texts]
 
 lda = LdaModel(
     common_corpus, 
@@ -47,6 +48,6 @@ lda = LdaModel(
 print(f"Perplexity: {lda.log_perplexity(common_corpus)}")
 
 coherence_lda = CoherenceModel(
-    model=lda, texts=tokenized_X_train,
+    model=lda, texts=tokenized_texts,
     dictionary=common_dictionary, coherence='c_v')
 print(f"Coherence: {coherence_lda.get_coherence()}")
